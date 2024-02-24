3ff720c76811e2f8c791e89cf83a2f80ffd7eb37
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-24 20:18:55 +0800

    feat(api): created default entry point of unexpected exception

---
+++ tribe-manager.api/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tribe_manager.api.Controllers
+{
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
