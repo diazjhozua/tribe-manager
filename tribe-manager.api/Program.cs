21565f28839e6e331eeaee2f9c5d5a6cc93d955b
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-24 16:06:07 +0800

    feat(api): register the dependency injection classes for app and infra

--- tribe-manager.api/Program.cs
+++ tribe-manager.api/Program.cs
@@ -1,5 +1,12 @@
+using tribe_manager.application;
+using tribe_manager.infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+
     builder.Services.AddControllers();
 }
 
