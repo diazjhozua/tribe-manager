01e674f1fb3b37ae587567556f45efd9aa65938b
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-24 19:16:11 +0800

    feat(app): implemented jwtToken create func in auth service

--- tribe-manager.application/Services/Authentication/AuthenticationService.cs
+++ tribe-manager.application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,34 @@
-﻿namespace tribe_manager.application.Services.Authentication
+﻿using tribe_manager.application.Common.Interfaces.Authentication;
+
+namespace tribe_manager.application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Login(string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), "Jhozua", "Diaz", email, "token");
+
+            Guid userId = Guid.NewGuid();
+            string token = _jwtTokenGenerator.GenerateToken(userId, "Jhozua", "Diaz");
+
+            return new AuthenticationResult(userId, "Jhozua", "Diaz", email, token);
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+            // TODO: Check if user already exists
+            // TODO: Create user (generate unique ID)
+
+            Guid userId = Guid.NewGuid();
+            string token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
         }
     }
 }
