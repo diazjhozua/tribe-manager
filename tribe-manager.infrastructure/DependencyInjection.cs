ac01cae497f023e0f8c1d6b2fb7eb50a26a2cde5
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-24 19:13:15 +0800

    feat(api/infra): injected JwtSettings class as config

--- tribe-manager.infrastructure/DependencyInjection.cs
+++ tribe-manager.infrastructure/DependencyInjection.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using tribe_manager.application.Common.Services;
+using tribe_manager.infrastructure.Authentication;
 using tribe_manager.infrastructure.Services;
 
 namespace tribe_manager.infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>(); 
             return services;
         }
