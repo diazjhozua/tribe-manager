421981d091686ef6ba1e5a1de3e5a4d4c27218ed
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-26 10:15:16 +0800

    feat(app): leverage ErrorOr package in AuthService

--- tribe-manager.application/Services/Authentication/IAuthenticationService.cs
+++ tribe-manager.application/Services/Authentication/IAuthenticationService.cs
@@ -1,8 +1,10 @@
-﻿namespace tribe_manager.application.Services.Authentication
+﻿using ErrorOr;
+
+namespace tribe_manager.application.Services.Authentication
 {
     public interface IAuthenticationService
     {
-        AuthenticationResult Login (string email, string password);
-        AuthenticationResult Register (string firstName, string lastName, string email, string password);
+        ErrorOr<AuthenticationResult> Login (string email, string password);
+        ErrorOr<AuthenticationResult> Register (string firstName, string lastName, string email, string password);
     }
 }
