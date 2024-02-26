c898945e5f315f8e97e110b546d12fdb9216553e
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-26 10:13:57 +0800

    feat(domain): created error classes for Auth and User

---
+++ tribe-manager.domain/Common/Errors/Errors.User.cs
@@ -0,0 +1,14 @@
+﻿using ErrorOr;
+
+namespace tribe_manager.domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static class User
+        {
+            public static Error DuplicateEmail => Error.Conflict(
+                code: "User.DuplicateEmail",
+                description: "Email is already in use.");
+        }
+    }
+}
