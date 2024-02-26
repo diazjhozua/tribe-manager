89f93deac17df027c5deb4c456c915d456cbe493
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-26 10:14:27 +0800

    feat(api): created custom base controller for handling error values

---
+++ tribe-manager.api/Controllers/ApiController.cs
@@ -0,0 +1,46 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace tribe_manager.api.Controllers
+{
+    public class ApiController : ControllerBase
+    {
+        protected IActionResult Problem(List<Error> errors)
+        {
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+            var firstError = errors[0];
+
+            return DefaultProblem(firstError);
+        }
+
+        private IActionResult DefaultProblem(Error firstError)
+        {
+            var statusCode = firstError.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return Problem(statusCode: statusCode, title: firstError.Description);
+        }
+
+        private IActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+
+    }
+}
