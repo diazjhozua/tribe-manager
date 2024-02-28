866a8da143b1af29d88f22aa0a733e8cfa03ff69
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-02-28 22:28:07 +0800

    feat(app): added Validator for RegisterCmd and LoginQry

---
+++ tribe-manager.application/Services/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace tribe_manager.application.Services.Authentication.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
---
+++ tribe-manager.application/Services/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace tribe_manager.application.Services.Authentication.Queries.Login
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
