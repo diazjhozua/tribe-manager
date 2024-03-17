3740db9493db6d703888a16a21e12d29be56601a
Jhozua Diaz <diaz.jhozua@gmail.com>
2024-03-17 16:54:47 +0800

    feat(api): created sample controller

---
+++ tribe-manager.api/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tribe_manager.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleController(ISender mediator, IMapper mapper) : ApiController(mediator, mapper)
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Array.Empty<string>());
+        }
+    }
+}
