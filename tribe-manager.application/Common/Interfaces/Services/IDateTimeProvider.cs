16c24431591d7153fd25090d01b39b3bcdc796bc
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-10-30 19:55:02 +0800

    feat(app/infra): Adds IDateTimeProvider interface and implementation
    Introduces an interface for accessing the current date and time.
    This enables easier testing and mocking of date/time-dependent logic.
    Provides a concrete implementation that returns the current UTC time.

---
+++ tribe-manager.application/Common/Interfaces/Services/IDateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace tribe_manager.application.Common.Interfaces.Services
+{
+    public interface IDateTimeProvider
+    {
+        DateTimeOffset Now { get; }
+    }
+}
---
+++ tribe-manager.infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using tribe_manager.application.Common.Interfaces.Services;
+
+namespace tribe_manager.infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTimeOffset Now => DateTime.UtcNow;
+    }
+}
