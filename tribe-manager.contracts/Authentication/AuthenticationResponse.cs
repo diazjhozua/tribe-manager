082711201d5f5948e61c947ebc4be8a4b7142126
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-10-30 19:35:20 +0800

    feat(cont): Adds authentication contracts
    Defines contracts for user authentication,
    including request and response models
    for registration and login.

---
+++ tribe-manager.contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace tribe_manager.contracts.Authentication
+{
+    public record AuthenticationResponse(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token);
+}
---
+++ tribe-manager.contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace tribe_manager.contracts.Authentication
+{
+    public record LoginRequest(
+        string Email,
+        string Password0);
+}
