9332faf0d2b22287032b87a833d95a2d897536d0
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-10-30 14:58:38 +0800

    refactor(api): Restructures application setup and execution
    Refactors the application's startup process by grouping
    service configurations and the main application
    execution within their respective code blocks.
    
    This improves code organization and readability.

--- tribe-manager.api/Program.cs
+++ tribe-manager.api/Program.cs
@@ -1,25 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+WebApplication app = builder.Build();
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
