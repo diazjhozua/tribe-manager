e164a15bb9c220a38fe766f658c09d4a492084a6
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-10-31 08:48:36 +0800

    feat(app): Adds JWT token generator interface.
    Defines an interface for generating JWT tokens.
    
    This interface abstracts the token generation logic,
    allowing for different implementations and improved
    testability in the authentication process.

---
+++ tribe-manager.application/Common/Interfaces/Authentication/IJwtTokenGenrator.cs
@@ -0,0 +1,7 @@
+﻿namespace tribe_manager.application.Common.Interfaces.Authentication
+{
+    public interface IJwtTokenGenrator
+    {
+        string GenerateToken(Guid userId, string firstName, string lastName);
+    }
+}
