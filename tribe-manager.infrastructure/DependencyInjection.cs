e324eb1b67aba66e02a1db4f913337dbf33f80da
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-10-31 11:24:42 +0800

    feat(infra): Configures JWT authentication
    Configures JWT authentication using JwtBearer.
    This includes binding JWT settings from configuration,
    adding a singleton for the JWT token generator, and setting
    up the authentication scheme with token validation parameters.

--- tribe-manager.infrastructure/DependencyInjection.cs
+++ tribe-manager.infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using tribe_manager.application.Common.Interfaces.Authentication;
 using tribe_manager.application.Common.Interfaces.Services;
 using tribe_manager.infrastructure.Authentication;
@@ -11,10 +13,37 @@ namespace tribe_manager.infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
         {
-            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-
-            services.AddSingleton<IJwtTokenGenrator, JwtTokenGenerator>();
+            services.AddAuthentication(configuration);
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();   
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuthentication(
+            this IServiceCollection services,
+            ConfigurationManager configuration)
+        {
+            JwtSettings jwtSettings = new();
+            configuration.Bind(JwtSettings.SectionName, jwtSettings);
+            
+            services.AddSingleton(Options.Create(jwtSettings));
+            services.AddSingleton<IJwtTokenGenrator, JwtTokenGenerator>();
+
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                    };
+                }); 
 
             return services;
         }
