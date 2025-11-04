750a519b4a506ef6da81c31d78c8ca0907fb634e
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-11-04 22:20:12 +0800

    feat(domain): Adds Task Priority and Status Enums
    Introduces enums for task priority and work item status,
    allowing for standardized representation and management
    of these attributes within the application.

---
+++ tribe-manager.domain/Task/Enums/TaskPriority.cs
@@ -0,0 +1,9 @@
+namespace tribe_manager.domain.Task.Enums;
+
+public enum TaskPriority
+{
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Critical = 4
+}
---
+++ tribe-manager.domain/Task/Enums/WorkItemStatus.cs
@@ -0,0 +1,10 @@
+namespace tribe_manager.domain.Task.Enums;
+
+public enum WorkItemStatus
+{
+    Pending = 1,
+    InProgress = 2,
+    InReview = 3,
+    Completed = 4,
+    Cancelled = 5
+}
