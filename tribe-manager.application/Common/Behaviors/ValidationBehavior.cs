947aa9b68c442cc7c94750e1c0cd78efb7cc2765
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-11-13 22:31:06 +0800

    feat(app): Adds validation behavior to MediatR pipeline
    Implements a validation behavior using FluentValidation that is added to the MediatR pipeline.
    
    This allows centralized validation logic for requests handled by MediatR. It checks for validators, executes them, and returns validation errors, if any, as part of the response.

---
+++ tribe-manager.application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,41 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace tribe_manager.application.Common.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null) :
+        IPipelineBehavior<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+            where TResponse : IErrorOr
+    {
+
+        private readonly IValidator<TRequest>? _validator = validator;
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+
+            if (_validator is null)
+            {
+                return await next(cancellationToken);
+            }
+
+            FluentValidation.Results.ValidationResult validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.IsValid)
+            {
+                return await next(cancellationToken);
+            }
+
+            List<Error> errors = validationResult.Errors
+                .ConvertAll(validationFailure => Error.Validation(
+                    validationFailure.PropertyName,
+                    validationFailure.ErrorMessage));
+
+            return (dynamic)errors;
+        }
+    }
+}
--- tribe-manager.application/DependencyInjection.cs
+++ tribe-manager.application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.Internal;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using tribe_manager.application.Common.Behaviors;
 
 namespace tribe_manager.application
 {
@@ -6,6 +10,13 @@ namespace tribe_manager.application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Extensions).Assembly));
+
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
+
             return services;
         }
     }
