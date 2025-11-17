1a24e394ca49276f795c4246cf7c8a1f64eb945d
Jhozua Diaz <diaz.jhozua@gmail.com>
2025-11-17 11:48:11 +0800

    feat(api): Adds authentication mapping configuration
    Configures Mapster to map between authentication result and response objects, streamlining the process of converting authentication data between the application and API layers.

---
+++ tribe-manager.api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using tribe_manager.application.Services.Authentication;
+using tribe_manager.contracts.Authentication;
+
+namespace tribe_manager.api.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>();
+        }
+    }
+}
